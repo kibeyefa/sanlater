40f7482e1b49492918e9b1e4c26c695fd4f7de26
Koboju Kibeyefa <kobojuezekiel@gmail.com>
2021-12-10 12:13:57 +0100

    changed form backend

---
+++ app.js
@@ -0,0 +1,44 @@
+let emailForm = document.forms[0];
+const body = document.body;
+const overlay = document.querySelector('.overlay');
+const modal = document.querySelector('.modal');
+const modal_error = document.querySelector('.modal__error');
+const modal_btn = document.querySelector('.modal__btn');
+const modalErrorBtn = document.querySelector('.modal__error__btn');
+
+modal_btn.addEventListener('click', () => {
+    overlay.classList.add('hide');
+    modal.style.display = 'none';
+    body.classList.remove('no_scroll');
+});
+
+modalErrorBtn.addEventListener('click', () => {
+    overlay.classList.add('hide');
+    modal_error.style.display = 'none';
+    body.classList.remove('no_scroll');
+});
+
+emailForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    let res = await fetch('http://127.0.0.1:8000/api/email/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: emailForm.email.value,
+        }),
+    });
+    console.log(res);
+
+    overlay.classList.remove('hide');
+    overlay.classList.remove('hide');
+    body.classList.add('no_scroll');
+
+    if (res.status === 201) {
+        modal.style.display = 'grid';
+    } else {
+        modal_error.style.display = 'grid';
+    }
+    emailForm.reset();
+});
